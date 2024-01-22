95e8f8a29905b400826a8a1d2c067c40b1bac8f6
Valentin Grozev <valentin.grozev@abv.bg>
2024-01-22 21:55:44 +0200

    Problem solution optimisation

--- 3_programming_advanced_with_python_january_2024/08_multidimensional_lists_exercise_two/4_easter_bunny.py
+++ 3_programming_advanced_with_python_january_2024/08_multidimensional_lists_exercise_two/4_easter_bunny.py
@@ -13,36 +13,36 @@ moves = {"up": [-1, 0], "down": [1, 0], "left": [0, -1], "right": [0, 1]}
 max_number_of_eggs = float('-inf')
 bunny_way = ""
 final_bunny_moves = []
+end_of_collecting = False
 
 for move in moves:
     current_eggs = 0
     current_bunny_moves = []
+    current_bunny_pos = bunny_position
+    end_of_collecting = False
     for row in range(size_of_field):
+        if end_of_collecting:
+            break
         for col in range(size_of_field):
-            next_row_position = bunny_position[0] + moves[move][0]
-            next_col_position = bunny_position[1] + moves[move][1]
+            next_row_position = current_bunny_pos[0] + moves[move][0]
+            next_col_position = current_bunny_pos[1] + moves[move][1]
             if 0 <= next_row_position < size_of_field and 0 <= next_col_position < size_of_field:
                 if field[next_row_position][next_col_position] == "X":
+                    end_of_collecting = True
                     break
                 else:
                     current_eggs += int(field[next_row_position][next_col_position])
                     current_bunny_moves.append([next_row_position, next_col_position])
-                    if move == "up":
-                        moves[move][0] += -1
-                    elif move == "down":
-                        moves[move][0] += 1
-                    elif move == "left":
-                        moves[move][1] += -1
-                    elif move == "right":
-                        moves[move][1] += 1
+                current_bunny_pos = [next_row_position, next_col_position]
             else:
+                end_of_collecting = True
                 break
+
     if current_eggs > max_number_of_eggs and current_bunny_moves:
         max_number_of_eggs = current_eggs
         final_bunny_moves = current_bunny_moves
         bunny_way = move
 
 print(bunny_way)
-for move in final_bunny_moves:
-    print(move)
+[print(move) for move in final_bunny_moves]
 print(max_number_of_eggs)
