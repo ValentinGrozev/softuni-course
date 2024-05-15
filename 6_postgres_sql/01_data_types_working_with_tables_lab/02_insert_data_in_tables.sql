cace975b9f4b8760f0edc449b66d4789d8a769fd
Valentin Grozev <valentin.grozev@abv.bg>
2024-05-15 23:45:31 +0300

    Add more solutions for postreSQL

---
+++ 6_postgres_sql/01_data_types_working_with_tables_lab/02_insert_data_in_tables.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees(first_name, last_name, hiring_date, salary, devices_number)
+VALUES
+	('Bruno', 'Fernandes', '2020-01-24', 10000.25, 8),
+	('Marcus', 'Rashford', '2016-04-22', 9999.85, 10),
+	('Kobbie', 'Mainoo', '2023-07-16', 875.37, 37);
