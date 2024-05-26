33dd311a0be5f6dc3c0710acef1f801a34f74fab
Valentin Grozev <valentin.grozev@abv.bg>
2024-05-26 20:45:41 +0300

    Remove redundant files

--- 6_postgres_sql/7_data_aggregations_lab/01_departments_info_by_id.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-	department_id,
-	count(*) AS employee_count
-FROM employees
-GROUP BY department_id
-ORDER BY department_id
--- 6_postgres_sql/7_data_aggregations_lab/07_filter_total_salaries.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	department_id,
-	sum(salary) AS "Total Salary"
-FROM
-	employees
-GROUP BY department_id
-HAVING sum(salary) < 4200
-ORDER BY department_id
